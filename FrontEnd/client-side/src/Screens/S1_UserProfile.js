0cc9c8bf70ba9217ca59311234ce5e6ec6c77ee3
kahitoz <kshitiz.sinha.025@gmail.com>
2023-10-24 23:05:15 +0530

    Now after login it will show name

--- FrontEnd/client-side/src/Screens/S1_UserProfile.js
+++ FrontEnd/client-side/src/Screens/S1_UserProfile.js
@@ -1,7 +1,20 @@
+import jwt_decode from "jwt-decode"
+import Cookies from "js-cookies"
+import { useState } from "react"
 const UserProfile = () =>{
+    const [name, setname] =useState("")
+    cosnt[l_name, setl_name] = useState("")
+    const token = Cookies.getItem("Access_token")
+    const decoded = jwt_decode(token)
+
+    console.log(decoded)
+    setname(decoded.given_name)
+    setl_name(decoded.family_name)
+    
+    const join = name +" "+l_name
     return(
         <>
-        <p>Hello World</p>
+        <p>Hello, {join}</p>
         </>
     )
 }
