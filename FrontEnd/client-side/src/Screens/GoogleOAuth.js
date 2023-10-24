5602a5d4c95c448bba985732c44c25b8c46a626e
kahitoz <kshitiz.sinha.025@gmail.com>
2023-10-24 23:04:55 +0530

    Stored the JWT key in Cooky

--- FrontEnd/client-side/src/Screens/GoogleOAuth.js
+++ FrontEnd/client-side/src/Screens/GoogleOAuth.js
@@ -1,7 +1,9 @@
     import { useEffect, useState } from "react";
     import { useNavigate } from "react-router-dom";
+    import Cookies from "js-cookies"
     const GoogleOAuth = () => {
     const [gwt, setgwt] = useState("");
+    const [counter, setcounter] = useState(0)
     function handleCallbackResponse(response) {
         try {
         if (response.error) {
@@ -11,7 +13,7 @@
         } catch (error) {}
     }
     console.log(gwt)
-
+    Cookies.setItem("Access_token", gwt)
     const client_id = process.env.REACT_APP_CLIENT_ID;
     useEffect(() => {
         /* global google */
@@ -29,9 +31,10 @@
     const now_navigate = () =>{
         navigate('/user/profile')
     }
+    
     useEffect(()=>{
         if(gwt){
-            now_navigate()
+            now_navigate()     
         }
     })
     return (
